76699aec90687309218be52109c50e6e1a888559
wangzx <wangzx@elab-plus.com>
2018-05-14 20:32:46 +0800

    MyWeb BEGIN

---
+++ MoHui/src/com/dh/ddfx/controller/DhTemplateController.java
@@ -0,0 +1,14 @@
+package com.dh.ddfx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by duh on 2018/5/14 20:26.
+ * Email:duh@elab-plus.com
+ * Version:v1.0
+ */
+@Controller
+@RequestMapping("")
+public class DhTemplateController {
+}
