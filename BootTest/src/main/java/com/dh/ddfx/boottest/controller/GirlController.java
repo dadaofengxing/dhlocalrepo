2175e2766097222e04abf0458acc49380ed0ba72
duhui <370489481@qq.com>
2018-06-18 14:47:53 +0800

    从yml获取配置值

--- BootTest/src/main/java/com/dh/ddfx/boottest/controller/GirlController.java
+++ BootTest/src/main/java/com/dh/ddfx/boottest/controller/GirlController.java
@@ -1,13 +1,23 @@
 package com.dh.ddfx.boottest.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GirlController {
 
+    @Value("${size}")
+    private String size;
+
     @RequestMapping(value = "girl",method = RequestMethod.GET)
     public String test(){
         return "b";
+    }
+
+    @RequestMapping("size")
+    public String getTheSize(){
+        return size;
     }
 }
