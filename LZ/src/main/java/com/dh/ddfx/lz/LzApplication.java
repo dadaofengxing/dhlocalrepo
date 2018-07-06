9880531b558fd89258e6799292ad68582a77af61
duh <duh@elab-plus.com>
2018-07-06 10:52:57 +0800

    初始化项目

---
+++ LZ/src/main/java/com/dh/ddfx/lz/LzApplication.java
@@ -0,0 +1,12 @@
+package com.dh.ddfx.lz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LzApplication.class, args);
+	}
+}
---
+++ LZ/src/main/java/com/dh/ddfx/lz/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.dh.ddfx.lz.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author duh
+ * @create 2018/7/6 10:48
+ * @email duh@elab-plus.com
+ **/
+@Controller
+public class ProductController {
+
+    public void list(){
+
+    }
+}
